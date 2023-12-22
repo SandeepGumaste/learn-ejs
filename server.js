ac44c2a39fedf522607e4314b17da276d64d7a48
Sandeep <sandeep@Workcells-MacBook-Air.local>
2023-12-22 15:42:05 +0530

    init

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var bodyParser=require('body-parser')
+var cors = require('cors')
+var path = require('path')
+
+var app = express();
+app.use(bodyParser())
+app.use(cors())
+
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
+app.get('/', function(req,res){
+    res.render('index');
+})
+
+app.listen(8000, function(){
+    console.log("heard on 8000");
+})

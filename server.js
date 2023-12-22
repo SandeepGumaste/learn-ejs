9e9be768465cd99cc7ad13e720fca1c7d114bbf4
Sandeep Gumaste <sandeep.gumaste@zeno.health>
2023-12-22 16:26:17 +0530

    injecting values

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ var cors = require('cors')
 var path = require('path')
 
 var app = express();
-app.use(bodyParser())
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(cors())
 
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
 
 app.get('/', function(req,res){
-    res.render('index');
+    res.render('index',{
+        foo:'bar'
+    });
 })
 
 app.listen(8000, function(){
